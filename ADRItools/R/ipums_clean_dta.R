0a973abc4f3923ccbf77ee8ea882a9d383136e94
Guy Abel <g.j.abel@gmail.com>
2020-08-23 14:58:15 +0800

    pacakge name change

---
+++ ADRItools/R/ipums_clean_dta.R
@@ -0,0 +1,22 @@
+#' Clean IPUMS International Data Imported from Stata File
+#'
+#' @param d Data frame with labels, as typically obtained when imported using haven::read_dta()
+#' @param numeric_convert Vector of character strings for columns to convert to numeric types
+#' @param string_as_factors Covert any factors to strings.
+#' @param clean_labels Clean labels. Extra step, often not required.
+#' @param mis_lab Vector of character strings to convert labels for missing values to `NA`
+#'
+#' @return Cleaned data frame.
+#' @export
+ipums_clean_dta <- function(
+  d = NULL, numeric_convert = NULL, string_as_factors = TRUE, clean_labels = FALSE,
+  mis_lab = c("unknown", "niu (not in universe)", "not reported/missing",
+              "unknown/missing", "response suppressed", "unclassifiable")
+){
+  d %>%
+    {if(clean_labels) dplyr::mutate_if(is.labelled, ipumsr::lbl_clean) else .} %>%
+    dplyr::mutate_if(is.labelled, ~ipumsr::lbl_na_if(., ~.lbl %in% mis_lab)) %>%
+    dplyr::mutate_at({numeric_convert}, haven::zap_labels) %>%
+    dplyr::mutate_if(is.labelled, haven::as_factor) %>%
+    {if(!string_as_factors) dplyr::mutate_if(., is.factor, as.character) else .}
+}
